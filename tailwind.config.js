67cf2bbc236a889fe0f4dd87bc838a18c6bd4457
Gentlewhiz <damolaaya@gmail.com>
2026-08-28 01:34:39 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./hotel-booking-confirmation-page-main/**/*.{html,js}", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
